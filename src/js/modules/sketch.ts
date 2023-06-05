c9c9e268ecba8a2113dda6c5e807517c54aaaaaa
Ryota Ohhashi <ryota.ohhashi@accenture.com>
2023-06-05 17:19:33 +0900

    modified

--- src/js/modules/sketch.ts
+++ src/js/modules/sketch.ts
@@ -1,7 +1,5 @@
 import p5 from "p5";
 
-// 画像の読み込み
-
 const sketch = (p: p5) => {
   let centerX: number;
   let centerY: number;
@@ -10,14 +8,13 @@ const sketch = (p: p5) => {
   let rotationSpeed: number;
   let isDragging: boolean;
   let dragStartTime: number;
-  // let dragStartAngle: number;
   let dragStartMouseX: number;
   let dragStartMouseY: number;
   let img: p5.Image;
 
   p.preload = () => {
     img = p.loadImage("/assets/img/handSpinner.png");
-  }
+  };
 
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
@@ -30,16 +27,12 @@ const sketch = (p: p5) => {
   };
 
   p.draw = () => {
-
-    // 回転速度の減衰
     if (!isDragging) {
       rotationSpeed *= 0.999;
     }
 
-    // 回転の更新
     rotation += rotationSpeed;
 
-    // ドラッグの時間と距離から回転数を計算
     if (isDragging) {
       const dragEndTime = p.millis();
       const dragDuration = dragEndTime - dragStartTime;
@@ -61,7 +54,6 @@ const sketch = (p: p5) => {
     if (d < shapeSize / 2) {
       isDragging = true;
       dragStartTime = p.millis();
-      // dragStartAngle = rotation;
       dragStartMouseX = p.mouseX;
       dragStartMouseY = p.mouseY;
     }
@@ -74,19 +66,18 @@ const sketch = (p: p5) => {
   const drawHandSpinner = () => {
     p.background(220);
 
-    // 画像の描画
     p.push();
     p.translate(centerX, centerY);
     p.rotate(rotation);
 
-    // 回転数に応じて画像の色を計算
-    const maxRotationSpeed = 0.1;
+    const maxRotationSpeed = 0.8;
     const red = p.map(Math.abs(rotationSpeed), 0, maxRotationSpeed, 50, 255);
-    p.tint(red, 0, 0); // 画像の色を設定
+    const blue = p.map(Math.abs(rotationSpeed), 0, maxRotationSpeed, 255, 50);
+    p.tint(red, 0, blue);
     p.imageMode(p.CENTER);
     p.image(img, 0, 0, shapeSize, shapeSize);
     p.pop();
-  }
+  };
 };
 
 export default sketch;
